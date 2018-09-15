c431f97d9b6373e68fb7a84706d2884349ae8810
Dzj <75314715@qq.com>
2018-09-15 23:33:37 +0800

    9.15

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
-# class Firstaddons(http.Controller):
-#     @http.route('/firstaddons/firstaddons/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class Firstaddons(http.Controller):
+    @http.route('/firstaddons/firstaddons/', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
 
-#     @http.route('/firstaddons/firstaddons/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('firstaddons.listing', {
-#             'root': '/firstaddons/firstaddons',
-#             'objects': http.request.env['firstaddons.firstaddons'].search([]),
-#         })
+    @http.route('/firstaddons/firstaddons/objects/', auth='public')
+    def list(self, **kw):
+        return http.request.render('firstaddons.listing', {
+            'root': '/firstaddons/firstaddons',
+            'objects': http.request.env['firstaddons.firstaddons'].search([]),
+        })
 
-#     @http.route('/firstaddons/firstaddons/objects/<model("firstaddons.firstaddons"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('firstaddons.object', {
-#             'object': obj
-#         })
+    @http.route('/firstaddons/firstaddons/objects/<model("firstaddons.firstaddons"):obj>/', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('firstaddons.object', {
+            'object': obj
+        })
